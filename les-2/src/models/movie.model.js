39fd1fe4e82a13bf18348066b077a33a615e6fcf
dnieuwmans <43491329+dnieuwmans@users.noreply.github.com>
2019-05-20 09:25:46 +0200

    Fix typo

--- les-2/src/models/movie.model.js
+++ les-2/src/models/movie.model.js
@@ -3,7 +3,7 @@ export default class Movie {
     title = '';
 
     // We can have multiple approaches for transforming data into a model.
-    // This is case one, the seond case will be handled in the user API.
+    // This is case one, the second case will be handled in the user API.
     static fromJSON(params) {
       const newMovie = new Movie();
 
